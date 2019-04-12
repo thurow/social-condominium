116e60d67d23aa4a04187dcd99736f702e0d2b97
Alexandre Thurow <alexandre@trezo.com.br>
2019-04-11 21:29:00 -0300

    adicionado componente para keyboard não sobrepor o campo

--- socialCondominium/src/App/Containers/LoginScreen.js
+++ socialCondominium/src/App/Containers/LoginScreen.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
-import { Text, TextInput, View, TouchableOpacity, Alert, Image } from 'react-native';
-
+import {
+	Animated,
+	Dimensions,
+	Keyboard,
+	Text,
+	TextInput,
+	View,
+	TouchableOpacity,
+	Alert,
+	Image,
+	UIManager,
+	TextInputState
+} from 'react-native';
+import KeyboardShift from './KeyboardShift';
 // Add Actions - replace 'Your' with whatever your reducer is called :)
 // import YourActions from '../Redux/YourRedux'
 
@@ -8,49 +20,51 @@ import { Text, TextInput, View, TouchableOpacity, Alert, Image } from 'react-nat
 import styles from './Styles/LoginScreenStyle';
 
 class LoginScreen extends Component {
-  state = {
-    email: '',
-    password: ''
-  }
+	state = {
+		email: '',
+		password: ''
+	};
 	render() {
 		return (
-			<View style={styles.container}>
-        <View style={styles.containerLogo}>
-          <Image source={require('../Images/logo_bg.png')} style={styles.logo} resizeMode="contain" />
-        </View>
-        <View style={styles.mainContainer}>
-				  <Text style={styles.titleText}>Entre no Condomínio Social</Text>
-          <TextInput
-            value={this.state.email}
-            name="email"
-            style={styles.textInput}
-            placeholder="email@provedor.com"
-            autoCapitalize="none"
-            keyboardType="email-address"
-            onChangeText={email => this.setState({email})}
-          />
-          <TextInput
-            value={this.state.password}
-            name="password"
-            style={styles.textInput}
-            placeholder="Sua Senha"
-            secureTextEntry
-            onChangeText={password => this.setState({password})}
-            onSubmitEditing={this._submitForm}
-          />
-          <TouchableOpacity onPress={this._submitForm}>
-            <Text style={styles.button}>Entrar</Text>
-          </TouchableOpacity>
-        </View>
-			</View>
+			<KeyboardShift>
+				{() => (
+					<View>
+						<View style={styles.containerLogo}>
+							<Image source={require('../Images/logo_bg.png')} style={styles.logo} resizeMode="contain" />
+						</View>
+						<Text style={styles.titleText}>Faça seu Login</Text>
+						<TextInput
+							value={this.state.email}
+							name="email"
+							style={styles.textInput}
+							placeholder="email@provedor.com"
+							autoCapitalize="none"
+							keyboardType="email-address"
+							onChangeText={(email) => this.setState({ email })}
+						/>
+						<TextInput
+							value={this.state.password}
+							name="password"
+							style={styles.textInput}
+							placeholder="Sua Senha"
+							secureTextEntry
+							onChangeText={(password) => this.setState({ password })}
+							onSubmitEditing={this._submitForm}
+						/>
+						<TouchableOpacity onPress={this._submitForm}>
+							<Text style={styles.button}>Entrar</Text>
+						</TouchableOpacity>
+					</View>
+				)}
+			</KeyboardShift>
 		);
-  }
+	}
 
-  _submitForm = () => {
-    const { email, password } = this.state
-    Alert.alert(email, password)
-    // do some stuff here…
-  };
+	_submitForm = () => {
+		const { email, password } = this.state;
+		Alert.alert(email, password);
+		// do some stuff here…
+	};
 }
 
 export default LoginScreen;
