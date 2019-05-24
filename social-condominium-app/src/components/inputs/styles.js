88cdc17d2be09160fe0e497fdaf9697129d30ca4
Alexandre Thurow <alexandre@trezo.com.br>
2019-05-24 00:45:04 -0300

    ajustes gerais cadastro social

--- social-condominium-app/src/components/inputs/styles.js
+++ social-condominium-app/src/components/inputs/styles.js
@@ -1,9 +1,11 @@
 import styled from 'styled-components/native'
 
 export const InputText = styled.TextInput`
-    height: 40px;
+    height: ${props => props.multiline ? "90px" : '40px'};
     margin-bottom: 10px;
+    ${props => props.multiline ? "margin-top: 10px" : ""};
+    ${props => props.multiline ? "align-items: flex-start" : ""};
     padding-bottom: 10px;
-    border-bottom-width: 1;
-    border-bottom-color: #3b5998;
+    ${props => props.multiline ? "border-width: 1" : "border-bottom-width: 1"};
+    ${props => props.multiline ? "border-color: #3b5998;" : "border-bottom-color: #3b5998;"};
 `
--- social-condominium-app/src/index.js
+++ social-condominium-app/src/index.js
@@ -1,17 +1,16 @@
-import { createStackNavigator } from 'react-navigation';
-import LoginScreen from './containers/login/LoginScreen';
-import RegisterScreen from './containers/cadastro/RegisterScreen'
-import DashboardScreen from './containers/dashboard/DashboardScreen'
-import SocialSpaceRegister from './containers/spaces/SocialSpaceRegister'
-import AsyncStorage from '@react-native-community/async-storage';
+// import { createStackNavigator } from 'react-navigation';
+// import LoginScreen from './containers/login/LoginScreen';
+// import RegisterScreen from './containers/cadastro/RegisterScreen'
+// import DashboardScreen from './containers/dashboard/DashboardScreen'
+// import SocialSpaceRegister from './containers/spaces/SocialSpaceRegister'
 
-const MainNavigator = createStackNavigator({
-  Home: LoginScreen,
-  Register: RegisterScreen,
-  Dashboard: DashboardScreen,
-  SocialSpaceRegister: SocialSpaceRegister
-}, {
-    headerMode: 'none'
-  });
+// const MainNavigator = createStackNavigator({
+//   Home: LoginScreen,
+//   Register: RegisterScreen,
+//   Dashboard: DashboardScreen,
+//   SocialSpaceRegister: SocialSpaceRegister
+// }, {
+//     headerMode: 'none'
+//   });
 
-export default { MainNavigator };
+// export default { MainNavigator };
