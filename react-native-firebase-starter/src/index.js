1d491e16d4e0d81e43bbb85467aeae72832d8bdf
Bryan Leite <bryan.leite@senior.com.br>
2019-06-17 21:09:52 -0300

    Desenvolvido tela de listagem de condomínios

--- react-native-firebase-starter/src/index.js
+++ react-native-firebase-starter/src/index.js
@@ -6,7 +6,7 @@ import SocialSpaceRegisterScreen from './containers/spaces/SocialSpaceRegisterSc
 import PostRegisterScreen from './containers/post/PostRegisterScreen';
 import CondominiumScreen from './containers/condominium/CondominiumScreen';
 import SocialSpaceListScreen from './containers/spaces/SocialSpaceListScreen';
-import { CondominiumListScreen } from './containers/condominium/CondominiumListScreen';
+import CondominiumListScreen from './containers/condominium/CondominiumListScreen';
 
 const MainNavigator = createStackNavigator({
   Home: LoginScreen,
